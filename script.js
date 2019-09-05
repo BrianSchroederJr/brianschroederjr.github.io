d6aaefd19ea31c197e55c6f201268f078537872c
Brian <48108069+BrianSchroederJr@users.noreply.github.com>
2019-09-05 14:13:14 -0700

    Portfolio Page Re-Design  Sep 2019

--- script.js
+++ script.js
@@ -1,117 +1,97 @@
+// Update years of experience
+document.getElementById("yearsOfExperience").innerText = (new Date().getFullYear() - 2007) + '';
 
-// Get Canvas
-var cnvs = document.getElementById("backCanvas");
-// Declare and initialize variables
-var winWidth = window.innerWidth;
-var winHeight = window.innerHeight;
-var starCnt = 1000;
-var starAreaOfInfluence = 400;
-var xDist;
-var yDist;
-var xDist2;
-var yDist2;
-var mag;
-var xNorm;
-var yNorm;
-var xMove;
-var yMove;
-var stars = [];
-
-// Get Canvas 2d Context
-var ctx = cnvs.getContext("2d");
-
-// Resize Canvas
-resizeCanvas();
-
-// Resize the Canvas when the window resizes
-window.addEventListener('resize' , resizeCanvas, false);
+// Upage copyright year
+document.getElementById("currYear").innerText = (new Date().getFullYear()) + '';
 
 
-// Resize Canvas to match window
-function resizeCanvas() {
-    winWidth = window.innerWidth;
-    winHeight = window.innerHeight;
-    cnvs.width = winWidth;
-    cnvs.height = winHeight;
 
-    // Create star field
-    buildStars();
-}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The JavaScript below handles the cursor and typing animation seen on the top left of this page.
+// Two different asynchronous calls to setInterval() and setTimeout()(called recursively) handle the 
+// blinking of the cursor and the random addition (and deletion) or characters.
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Move stars when the mouse moves
-function mouseMove(event) {
-    drawStarsMove(event.clientX, event.clientY);
-}
+// Get text span
+var textSpan = document.getElementById("HelloWorldText");
 
-// Create a random star field - random position, radius, hue, saturation, lightness, and alpha within some bounds that I thought looked good
-function buildStars() {
-    stars = [];
-    for(i=0; i<starCnt; i++) {
-        stars.push(
-            {
-                x: Math.random() * winWidth,
-                y: Math.random() * winHeight,
-                r: 0.4 + (Math.random()*1.5),
-                h: 195 + (Math.random()*90),
-                s: Math.random() * 100,
-                l: 70,
-                a: 0.4 + (Math.random()-0.25)
-            }
-        );
+// Variable declarations
+var completedTyping = false;
+var charsTyped = 0;
+var messageToType = 'Hello, World...';
+var blinkOn = false;
+
+
+// Update cursor function
+function udpateCursor() {
+    // Flip completed flag
+    blinkOn = !blinkOn;
+
+    if(blinkOn) {
+        textSpan.innerText += '█';
     }
-
-    // Draw static star field
-    drawStars();
-}
-
-// Draw stars - static (Used when first loaded or when the window is resized.)
-function drawStars() {
-    ctx.clearRect(0, 0, winWidth, winHeight);
-    for(i=0; i<starCnt; i++) {
-        ctx.beginPath();
-        ctx.arc(stars[i].x, stars[i].y, stars[i].r, 0 ,360);
-        ctx.fillStyle = "hsla("+ stars[i].h + "," + stars[i].s + "%," + stars[i].l + "%," + stars[i].a +")";
-        ctx.fill();
-        ctx.closePath();
-    }
-}
-
-// Draw stars - dynamic with mouse movement
-function drawStarsMove(mX, mY) {
-    // Clear Canvas
-    ctx.clearRect(0, 0, winWidth, winHeight);
-
-    // Redraw all stars
-    for(i=0; i<starCnt; i++) {
-        // Calculate distance to mouse pointer
-        xDist = mX - stars[i].x;
-        yDist = mY - stars[i].y;
-        xDist2 = xDist * xDist;
-        yDist2 = yDist * yDist;
-        mag = Math.sqrt(xDist2 + yDist2);
-        xNorm = xDist / mag;
-        yNorm = yDist / mag;
-        xMove = xNorm * (1/mag) * 450;
-        yMove = yNorm * (1/mag) * 450;
+    else {
         
-        // Draw static stars (all stars outside of the area of influence)
-        if(mag > starAreaOfInfluence) {
-            // Draw star - Filled circle using hsla() color for fill style
-            ctx.beginPath();
-            ctx.arc(stars[i].x, stars[i].y, stars[i].r, 0 ,360);
-            ctx.fillStyle = "hsla("+ stars[i].h + "," + stars[i].s + "%," + stars[i].l + "%," + stars[i].a +")";
-            ctx.fill();
-            ctx.closePath();
-        }
-        // Draw adjusted stars
-        else {
-            // Draw star - With adjusted x and y position to create visual affect around the mouse cursor
-            ctx.beginPath();
-            ctx.arc(stars[i].x + xMove, stars[i].y + yMove, stars[i].r, 0 ,360);
-            ctx.fillStyle = "hsla("+ stars[i].h + "," + stars[i].s + "%," + stars[i].l + "%," + stars[i].a +")";
-            ctx.fill();
-            ctx.closePath();
-        }
-
+        textSpan.innerText = textSpan.innerText.substring(0, textSpan.innerText.length - 1);
     }
+}   // End updateCursor()
+
+
+// Start the Blinking cursor by calling updateCursor() every 500ms infinitely
+window.setInterval(udpateCursor, 500);
+
+
+// Update text function
+function udpateText() {
+    
+    // Type out message if not completed
+    if (!completedTyping) {
+        // Increment character typed counter
+        charsTyped++;
+
+        // Add typed characters to display
+        if (charsTyped <= messageToType.length) {     
+            textSpan.innerText = messageToType.substring(0, charsTyped);
+            // Add cursor if blink On
+            if(blinkOn) {
+                textSpan.innerText += '█';
+            }
+        } 
+        // Message has been fully typed
+        else {
+            // Delay via iteration count before considering completed
+            if(charsTyped > messageToType.length + 18) {
+                completedTyping = true;
+                charsTyped = messageToType.length;
+            }
+        }
+    }   // End if not completed
+    // Else If completed
+    else {
+        // Decrement character typed counter
+        charsTyped--;
+
+        // Delete characters from display
+        if (charsTyped >= 0) {     
+            textSpan.innerText = messageToType.substring(0, charsTyped);
+            // Add cursor if blink On
+            if(blinkOn) {
+                textSpan.innerText += '█';
+            }
+        }
+        // Message has been fully deleted
+        else {
+            // Delay via iteration count before considering NOT completed and restarting
+            if(charsTyped < -7) {
+                completedTyping = false;
+                charsTyped = 0;
+            }
+        }
+    }   // End If completed
 }
+
+// Randomly Update text via this Immediately Invoked Function Expression(IIFE) that calls itself after each call to updateText()
+(function iterate() {
+    var timeRand = Math.round(Math.random() * (450 - 25)) + 25;
+    setTimeout(function() {udpateText(); iterate();}, timeRand);
+})();
